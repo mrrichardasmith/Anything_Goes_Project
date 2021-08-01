d476d7fe503187119563b1c89586b2977fda1c41
piercy87 <72551589+piercy87@users.noreply.github.com>
2021-08-01 09:11:38 +0100

    initial commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,28 @@
-import './App.css';
-import faker from  'faker'; 
-import ChartLibrary from 'chart.js/auto';
+import "./App.css";
+import faker from "faker";
+import ChartLibrary from "chart.js/auto";
 
-import BarChart from '../BarChart/BarChart';
-import Table from '../Table/Table';
-import RadarChart from '../RadarChart/RadarChart';
-
+import BarChart from "../BarChart/BarChart";
+import Table from "../Table/Table";
+import RadarChart from "../RadarChart/RadarChart";
+import Nav from "../NavBar/Nav";
 
 function App() {
-  
   return (
-    <div className="container">
-      <div className="header">
+    <div>
+      <Nav />
+      <div className="container">
+        <div className="header">
           <h1 className="title">The Anything Goes Project</h1>
-          <h3 className="description">A project space for collaboration and practicing skill gaps.</h3>
+          <h3 className="description">
+            A project space for collaboration and practicing skill gaps.
+          </h3>
           <br />
+        </div>
+        <BarChart />
+        <RadarChart />
+        <Table />
       </div>
-      <BarChart />
-      <RadarChart />
-      <Table />
-
     </div>
   );
 }
